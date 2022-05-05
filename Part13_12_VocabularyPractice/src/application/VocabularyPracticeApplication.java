4054bd5fc3e5ad2e0f8bc6dd739f5134e9d693d6
Chloe Lee-Hone <chloe.leehone@gmail.com>
2022-05-05 14:53:10 -0400

    Add VocabularyPracticeApplication, Dictionary, InputView, PracticeApplication, and PracticeView classes

---
+++ Part13_12_VocabularyPractice/src/application/VocabularyPracticeApplication.java
@@ -0,0 +1,11 @@
+package application;
+ 
+import static javafx.application.Application.launch;
+ 
+public class VocabularyPracticeApplication {
+ 
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        launch(PracticeApplication.class);                
+    }
+}
