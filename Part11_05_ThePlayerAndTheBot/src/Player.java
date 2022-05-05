2f4a1a2d15afbe8de4a84f87a3f54a387368975e
Chloe Lee-Hone <chloe.leehone@gmail.com>
2022-05-05 12:47:29 -0400

    Add Player, Bot, and Program classes

---
+++ Part11_05_ThePlayerAndTheBot/src/Player.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+/**
+ *
+ * @author chloe
+ */
+public class Player {
+	
+    private String name;
+    
+    public Player() { 
+    }
+    
+    public void play() {  
+    }
+    
+    public void printName() {
+    }
+}
