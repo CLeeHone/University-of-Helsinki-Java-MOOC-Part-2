ff375aab7266f7faaa273270148a2d5f7107eaa6
Chloe Lee-Hone <chloe.leehone@gmail.com>
2022-05-05 12:53:45 -0400

    Add A, B, C, D, E, IA, IB, IC, and Program classes

---
+++ Part11_07_BiggerClassDiagram/src/E.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+/**
+ *
+ * @author chloe
+ */
+import java.util.List;
+ 
+public class E extends C {
+    private List<C> elementC;
+}
---
+++ Part11_07_BiggerClassDiagram/src/IA.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+/**
+ *
+ * @author chloe
+ */
+public interface IA {
+    D elementD = new D();
+    
+    public void getName();
+}
